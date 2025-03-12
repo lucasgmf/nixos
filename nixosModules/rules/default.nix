258573c380f54512f85e7ac9355dc223b64260d9
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-03-12 21:04:27 +0000

    68 current  2025-03-12 21:04:26  25.05.20250309.e3e32b6  6.13.6                          *

--- nixosModules/rules/default.nix
+++ nixosModules/rules/default.nix
@@ -2,9 +2,9 @@
 {
   services.udev = {
     packages = with pkgs; [
-      # picoprobe-udev-rules # probe-rs udev rules
-      # platformio-core.udev # platformio
-      # openocd # platformio
+      picoprobe-udev-rules # probe-rs udev rules
+      platformio-core.udev # platformio
+      openocd # platformio
     ];
     extraRules = '''';
   };
