7878355f46c4fdcd6a8ae47f530a810405efb24f
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-03-21 02:46:16 +0000

    70 current  2025-03-21 02:46:16  25.05.20250309.e3e32b6  6.13.6                          *

--- nixosModules/stylix/default.nix
+++ nixosModules/stylix/default.nix
@@ -3,14 +3,16 @@
   lib,
   config,
   ...
-}: let
+}:
+let
   sakura = "${pkgs.base16-schemes}/share/themes/sakura.yaml";
-in {
+in
+{
   options = {
     autoStyling = {
       enable = lib.mkEnableOption "enables stylix auto styling";
-      colorScheme = lib.mkOption {default = sakura;};
-      image = lib.mkOption {default = ./default.png;};
+      colorScheme = lib.mkOption { default = sakura; };
+      image = lib.mkOption { default = ./default.png; };
     };
   };
 
