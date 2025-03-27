f30275c28bf73539de87fda7edc2d84a02d4885e
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-03-27 17:29:33 +0000

    109 current  2025-03-27 17:29:32  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -1,5 +1,5 @@
 require("rose-pine").setup({
-    variant = "auto", -- auto, main, moon, or dawn
+    variant = "auto",      -- auto, main, moon, or dawn
     dark_variant = "main", -- main, moon, or dawn
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
@@ -7,7 +7,7 @@ require("rose-pine").setup({
     enable = {
         terminal = true,
         legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-        migrations = true, -- Handle deprecated options automatically
+        migrations = true,        -- Handle deprecated options automatically
     },
 
     styles = {
@@ -55,8 +55,8 @@ require("rose-pine").setup({
         -- },
     },
 
-	-- NOTE: Highlight groups are extended (merged) by default. Disable this
-	-- per group via `inherit = false`
+    -- NOTE: Highlight groups are extended (merged) by default. Disable this
+    -- per group via `inherit = false`
     highlight_groups = {
         -- Comment = { fg = "foam" },
         -- StatusLine = { fg = "love", bg = "love", blend = 15 },
@@ -78,6 +78,6 @@ require("rose-pine").setup({
 })
 
 -- vim.cmd("colorscheme rose-pine")
--- vim.cmd("colorscheme rose-pine-main")
-vim.cmd("colorscheme rose-pine-moon")
+vim.cmd("colorscheme rose-pine-main")
+-- vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd("colorscheme rose-pine-dawn")
