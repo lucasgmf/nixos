926998f7a9f514c0201c4bd75823ba5986671c6c
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-03-27 16:03:58 +0000

    93 current  2025-03-27 16:03:56  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -1,5 +1,5 @@
 require "catppuccin".setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = "latte", -- latte, frappe, macchiato, mocha
     transparent_background = false, -- disables setting the background color.
     show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
     term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
@@ -43,4 +43,4 @@ require "catppuccin".setup({
     },
 })
 
-vim.cmd.colorscheme("nightfly")
+vim.cmd.colorscheme("gruvbox")
