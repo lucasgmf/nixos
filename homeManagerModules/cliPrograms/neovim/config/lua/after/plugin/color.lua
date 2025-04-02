3c45aefee89f91a8e545b0bed6e03cd89b310585
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-02 19:32:15 +0100

    130 current  2025-04-02 19:32:14  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -1,22 +1,22 @@
 require("catppuccin").setup({
     flavour = "auto", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
+    background = {    -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = true, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    transparent_background = false, -- disables setting the background color.
+    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
+        enabled = false,            -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
+    no_italic = false,              -- Force no italic
+    no_bold = false,                -- Force no bold
+    no_underline = false,           -- Force no underline
+    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },    -- Change the style of comments
         conditionals = { "italic" },
         loops = {},
         functions = {},
@@ -30,7 +30,34 @@ require("catppuccin").setup({
         operators = {},
         -- miscs = {}, -- Uncomment to turn off hard-coded styles
     },
-    color_overrides = {},
+    color_overrides = {
+        rosewater = "#F5B8AB",
+        flamingo = "#F29D9D",
+        pink = "#AD6FF7",
+        mauve = "#FF8F40",
+        red = "#E66767",
+        maroon = "#EB788B",
+        peach = "#FAB770",
+        yellow = "#FACA64",
+        green = "#70CF67",
+        teal = "#4CD4BD",
+        sky = "#61BDFF",
+        sapphire = "#4BA8FA",
+        blue = "#00BFFF",
+        lavender = "#00BBCC",
+        text = "#C1C9E6",
+        subtext1 = "#A3AAC2",
+        subtext0 = "#8E94AB",
+        overlay2 = "#7D8296",
+        overlay1 = "#676B80",
+        overlay0 = "#464957",
+        surface2 = "#3A3D4A",
+        surface1 = "#2F313D",
+        surface0 = "#1D1E29",
+        base = "#0b0b12",
+        mantle = "#11111a",
+        crust = "#191926",
+    },
     custom_highlights = {},
     default_integrations = true,
     integrations = {
