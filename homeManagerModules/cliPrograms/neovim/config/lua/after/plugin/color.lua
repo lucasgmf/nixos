c5873c17804397b085500e92ad3ae0e5bc268374
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-02 18:23:30 +0100

    121 current  2025-04-02 18:23:29  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -78,6 +78,7 @@ require("rose-pine").setup({
 })
 
 -- vim.cmd("colorscheme rose-pine")
--- vim.cmd("colorscheme rose-pine-main")
+vim.cmd("colorscheme rose-pine-main")
 -- vim.cmd("colorscheme rose-pine-moon")
-vim.cmd("colorscheme rose-pine-dawn")
+-- vim.cmd("colorscheme rose-pine-dawn")
+
