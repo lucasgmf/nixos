87d7ab3adf02e13bb283e15be0f7c248d1c95ea6
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-02 19:40:52 +0100

    132 current  2025-04-02 19:40:51  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -30,21 +30,22 @@ require("catppuccin").setup({
         operators = {},
         -- miscs = {}, -- Uncomment to turn off hard-coded styles
     },
-color_overrides = {
-    mocha = {
-      text = "#F4CDE9",
-      subtext1 = "#DEBAD4",
-      subtext0 = "#C8A6BE",
-      overlay2 = "#B293A8",
-      overlay1 = "#9C7F92",
-      overlay0 = "#866C7D",
-      surface2 = "#705867",
-      surface1 = "#5A4551",
-      surface0 = "#44313B",
+    color_overrides = {
+        mocha = {
+            text = "#F4CDE9",
+            subtext1 = "#DEBAD4",
+            subtext0 = "#C8A6BE",
+            overlay2 = "#B293A8",
+            overlay1 = "#9C7F92",
+            overlay0 = "#866C7D",
+            surface2 = "#705867",
+            surface1 = "#5A4551",
+            surface0 = "#44313B",
 
-      base = "#352939",
-      mantle = "#211924",
-      crust = "#1a1016",
+            base = "#352939",
+            mantle = "#211924",
+            crust = "#1a1016",
+        },
     },
     custom_highlights = {},
     default_integrations = true,
