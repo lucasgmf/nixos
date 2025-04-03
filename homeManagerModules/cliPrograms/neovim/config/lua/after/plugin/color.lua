a67a6049896c8c0c136183e8001b1ba8b8479835
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-04-03 01:35:02 +0100

    148 current  2025-04-03 01:35:01  25.05.20250318.b6eaf97  6.13.7                          *

--- homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/plugin/color.lua
@@ -1,67 +1,84 @@
-require("catppuccin").setup({
-    flavour = "auto", -- latte, frappe, macchiato, mocha
-    background = {    -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-    term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false,            -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15,          -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false,              -- Force no italic
-    no_bold = false,                -- Force no bold
-    no_underline = false,           -- Force no underline
-    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" },    -- Change the style of comments
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-    },
-    color_overrides = {
-        mocha = {
-            text = "#F4CDE9",
-            subtext1 = "#DEBAD4",
-            subtext0 = "#C8A6BE",
-            overlay2 = "#B293A8",
-            overlay1 = "#9C7F92",
-            overlay0 = "#866C7D",
-            surface2 = "#705867",
-            surface1 = "#5A4551",
-            surface0 = "#44313B",
+require("rose-pine").setup({
+    variant = "moon",      -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
 
-            base = "#352939",
-            mantle = "#211924",
-            crust = "#1a1016",
-        },
+    enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true,        -- Handle deprecated options automatically
     },
-    custom_highlights = {},
-    default_integrations = true,
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+
+    styles = {
+        bold = true,
+        italic = true,
+        transparency = false,
     },
+
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+    },
+
+    palette = {
+        -- Override the builtin palette per variant
+        -- moon = {
+        --     base = '#18191a',
+        --     overlay = '#363738',
+        -- },
+    },
+
+    -- NOTE: Highlight groups are extended (merged) by default. Disable this
+    -- per group via `inherit = false`
+    highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+        -- Visual = { fg = "base", bg = "text", inherit = false },
+    },
+
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
 })
 
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
+-- vim.cmd("colorscheme rose-pine")
+-- vim.cmd("colorscheme rose-pine-main")
+vim.cmd("colorscheme rose-pine-moon")
+-- vim.cmd("colorscheme rose-pine-dawn")
+
