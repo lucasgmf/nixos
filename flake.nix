80df07615898def2735e82a5fb687df411eb005b
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-11-07 22:26:39 +0000

    253 current  2025-11-07 22:26:38  25.05.20250318.b6eaf97  6.13.7                           *

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # lix-module = {
+      # url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -21,7 +21,7 @@
     {
       self,
       nixpkgs,
-      lix-module,
+      # lix-module,
       ...
     }@inputs:
     let
@@ -61,7 +61,7 @@
           };
 
           modules = [
-            lix-module.nixosModules.default
+            # lix-module.nixosModules.default
             ./hosts/nix-laptop
           ] ++ homeConfig ./homeManagerModules/laptop.nix;
         };
