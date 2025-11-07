b2c53245068bda77da324e442fdc15651a933d38
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-11-07 22:58:04 +0000

    256 current  2025-11-07 22:57:31  25.05.20250318.b6eaf97  6.13.7                           *

--- homeManagerModules/cliPrograms/scripts/update.nix
+++ homeManagerModules/cliPrograms/scripts/update.nix
@@ -2,17 +2,14 @@
 {
   home.packages = [
     (pkgs.writeShellScriptBin "update" ''
-      function reset_flake {
+      reset_flake() {
         pushd ~/nixos/
         git reset --hard
         popd
       }
-
       trap reset_flake ERR
-
       echo "NixOS Updating..."
       nh os switch --update --ask
-
       pushd ~/nixos/
       gen=$(nixos-rebuild list-generations | grep current)
       git commit -am "$gen"
