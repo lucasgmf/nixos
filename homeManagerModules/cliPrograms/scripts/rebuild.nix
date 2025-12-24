9ee3d1d80f2215b522676639cc9092bc1faef654
lucasgmf <lucasgalvaomfreitas@gmail.com>
2025-12-24 17:49:50 +0000

    292 current  2025-12-24 17:49:50  26.05.20251221.a653104 (Yarara)  6.17.9

--- homeManagerModules/cliPrograms/scripts/rebuild.nix
+++ homeManagerModules/cliPrograms/scripts/rebuild.nix
@@ -8,6 +8,7 @@
       git add .
       git diff --cached -U0 *.nix
       echo "NixOS Rebuilding..."
+      echo "Dahyun heart"
       nh os switch --ask
       gen=$(readlink /nix/var/nix/profiles/system | grep -oP 'system-\K\d+')
       git commit -m "$gen current  $(date '+%Y-%m-%d %H:%M:%S')  $(nixos-version)  $(uname -r)"
