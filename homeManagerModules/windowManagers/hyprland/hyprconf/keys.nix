f2f90259c6359f4394acb4bf921c9cc8e2843ff4
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-01-23 22:14:55 +0000

    331 current  2026-01-23 22:14:55  26.05.20251221.a653104 (Yarara)  6.18.2

--- homeManagerModules/windowManagers/hyprland/hyprconf/keys.nix
+++ homeManagerModules/windowManagers/hyprland/hyprconf/keys.nix
@@ -9,62 +9,76 @@
 	bind = [
 
           # Keybindings
-          "bind = $mainMod, Q, exec, $terminal"
-          "bind = $mainMod, C, killactive,"
-          "bind = $mainMod, M, exit,"
-          "bind = $mainMod, E, exec, $fileManager"
-          "bind = $mainMod, V, togglefloating,"
-          "bind = $mainMod, R, exec, $menu"
-          "bind = $mainMod, P, pseudo,"
-          "bind = $mainMod, J, togglesplit,"
+          "$mainMod, Q, exec, $terminal"
+          "$mainMod, C, killactive,"
+          "$mainMod, M, exit,"
+          "$mainMod, E, exec, $fileManager"
+          "$mainMod, V, togglefloating,"
+          "$mainMod, R, exec, $menu"
+          "$mainMod, P, pseudo,"
+          "$mainMod, J, togglesplit,"
           
           # Move focus
-          "bind = $mainMod, left, movefocus, l"
-          "bind = $mainMod, right, movefocus, r"
-          "bind = $mainMod, up, movefocus, u"
-          "bind = $mainMod, down, movefocus, d"
+          "$mainMod, left, movefocus, l"
+          "$mainMod, right, movefocus, r"
+          "$mainMod, up, movefocus, u"
+          "$mainMod, down, movefocus, d"
           
           # Switch workspaces
-          "bind = $mainMod, 1, workspace, 1"
-          "bind = $mainMod, 2, workspace, 2"
-          "bind = $mainMod, 3, workspace, 3"
-          "bind = $mainMod, 4, workspace, 4"
-          "bind = $mainMod, 5, workspace, 5"
-          "bind = $mainMod, 6, workspace, 6"
-          "bind = $mainMod, 7, workspace, 7"
-          "bind = $mainMod, 8, workspace, 8"
-          "bind = $mainMod, 9, workspace, 9"
-          "bind = $mainMod, 0, workspace, 10"
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
           
           # Move windows to workspaces
-          "bind = $mainMod SHIFT, 1, movetoworkspace, 1"
-          "bind = $mainMod SHIFT, 2, movetoworkspace, 2"
-          "bind = $mainMod SHIFT, 3, movetoworkspace, 3"
-          "bind = $mainMod SHIFT, 4, movetoworkspace, 4"
-          "bind = $mainMod SHIFT, 5, movetoworkspace, 5"
-          "bind = $mainMod SHIFT, 6, movetoworkspace, 6"
-          "bind = $mainMod SHIFT, 7, movetoworkspace, 7"
-          "bind = $mainMod SHIFT, 8, movetoworkspace, 8"
-          "bind = $mainMod SHIFT, 9, movetoworkspace, 9"
-          "bind = $mainMod SHIFT, 0, movetoworkspace, 10"
+          "$mainMod SHIFT, 1, movetoworkspace, 1"
+          "$mainMod SHIFT, 2, movetoworkspace, 2"
+          "$mainMod SHIFT, 3, movetoworkspace, 3"
+          "$mainMod SHIFT, 4, movetoworkspace, 4"
+          "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
           
           # Special workspace
-          "bind = $mainMod, S, togglespecialworkspace, magic"
-          "bind = $mainMod SHIFT, S, movetoworkspace, special:magic"
+          "$mainMod, S, togglespecialworkspace, magic"
+          "$mainMod SHIFT, S, movetoworkspace, special:magic"
           
           # Scroll through workspaces
-          "bind = $mainMod, mouse_down, workspace, e+1"
-          "bind = $mainMod, mouse_up, workspace, e-1"
-          
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+
+	  ];
+
+	  bindm = [
           # Move/resize windows
-          "bindm = $mainMod, mouse:272, movewindow"
-          "bindm = $mainMod, mouse:273, resizewindow"
+          "$mainMod, mouse:272, movewindow"
+          "$mainMod, mouse:273, resizewindow"
+	  ];
           
-          # Media controls
-          "bindl = , XF86AudioNext, exec, playerctl next"
-          "bindl = , XF86AudioPause, exec, playerctl play-pause"
-          "bindl = , XF86AudioPlay, exec, playerctl play-pause"
-          "bindl = , XF86AudioPrev, exec, playerctl previous"
+          bindl = [
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"
+	  ];
+
+	  bindel = [
+
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ", XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+          ", XF86MonBrightnessDown, exec, brightnessctl s 10%-"
 	  ];
 	};
 }
