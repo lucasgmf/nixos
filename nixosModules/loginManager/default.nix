52f68558b3bd589e7aec531c0136458456cc40f7
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-01-27 19:45:58 +0000

    398 current  2026-01-27 19:45:58  26.05.20251221.a653104 (Yarara)  6.18.2

--- nixosModules/loginManager/default.nix
+++ nixosModules/loginManager/default.nix
@@ -3,15 +3,41 @@
   lib,
   config,
   ...
-}: {
+}: let
+  custom-sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "hyprland_kath";
+    #themeConfig = {
+    #  Background = "path/to/background.jpg";
+    #  Font = "M+1 Nerd Font";
+    #};
+  };
+in {
   options = {
     customSDDM.enable = lib.mkEnableOption "enables custom configuration of sddm";
   };
-
+  
   config = lib.mkIf config.customSDDM.enable {
-
-  # enable major tooklits
-  gtk.enable = true;
-  qt.enable = true;
+    services.displayManager.sddm = {
+      enable = true;
+      # experimental support
+      wayland.enable = true;
+      
+      extraPackages = with pkgs; [
+        custom-sddm-astronaut
+      ];
+      
+      theme = "sddm-astronaut-theme";
+      
+      settings = {
+        Theme = {
+          Current = "sddm-astronaut-theme";
+        };
+      };
+    };
+    
+    environment.systemPackages = with pkgs; [
+      custom-sddm-astronaut
+      kdePackages.qtmultimedia
+    ];
   };
 }
