24cbd5b7d8070056a773aa1f13c51983d5b8cc93
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-01-27 12:32:48 +0000

    378 current  2026-01-27 12:32:48  26.05.20251221.a653104 (Yarara)  6.18.2

--- homeManagerModules/windowManagers/hyprland/hyprconf/keys.nix
+++ homeManagerModules/windowManagers/hyprland/hyprconf/keys.nix
@@ -8,7 +8,7 @@
 			drag_3fg = 1; # 3 fingers drag to move windows + super
 		};
 		# kb_options = caps:swapescape;
-		kb_options = compose:ralt;
+		kb_options = compose:rctrl;
 	};
 
 	# 4 finger swipe to change between workspaces
--- homeManagerModules/windowManagers/hyprland/hyprconf/xcompose.nix
+++ homeManagerModules/windowManagers/hyprland/hyprconf/xcompose.nix
@@ -4,13 +4,13 @@ let
   xcomposeFile = pkgs.writeText "XCompose" ''
     include "%L"
     
-    <Multi_key> <a> <a> : "ä" U00E4  # a umlaut lowercase
-    <Multi_key> <A> <A> : "Ä" U00C4  # A umlaut uppercase
-    <Multi_key> <o> <o> : "ö" U00F6  # o umlaut lowercase
-    <Multi_key> <O> <O> : "Ö" U00D6  # O umlaut uppercase
-    <Multi_key> <u> <u> : "ü" U00FC  # u umlaut lowercase
-    <Multi_key> <U> <U> : "Ü" U00DC  # U umlaut uppercase
-    <Multi_key> <s> <s> : "ß" U00DF  # sharp s;
+    <Multi_key> <a> : "ä" U00E4  # a umlaut lowercase
+    <Multi_key> <A> : "Ä" U00C4  # A umlaut uppercase
+    <Multi_key> <o> : "ö" U00F6  # o umlaut lowercase
+    <Multi_key> <O> : "Ö" U00D6  # O umlaut uppercase
+    <Multi_key> <u> : "ü" U00FC  # u umlaut lowercase
+    <Multi_key> <U> : "Ü" U00DC  # U umlaut uppercase
+    <Multi_key> <s> : "ß" U00DF  # sharp s;
     '';
 in
 {
