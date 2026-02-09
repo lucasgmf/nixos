50724bbbf978e48f756fe5c537e90881be9f0693
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-02-09 15:27:50 +0000

    521 current  2026-02-09 15:27:50  26.05.20260204.00c21e4 (Yarara)  6.18.8

--- homeManagerModules/cliPrograms/neovim/config/lua/after/init.lua
+++ homeManagerModules/cliPrograms/neovim/config/lua/after/init.lua
@@ -2,8 +2,9 @@ require("after.plugin.color")
 require("after.plugin.gitsigns")
 require("after.plugin.harpoon")
 require("after.plugin.lualine")
+require("after.plugin.mini") -- TODO: see this later 
+require("after.plugin.startify")
 require("after.plugin.telescope")
-require("after.plugin.mini")
 require("nvim-treesitter").setup{
     highlight = {
         enable = true,
