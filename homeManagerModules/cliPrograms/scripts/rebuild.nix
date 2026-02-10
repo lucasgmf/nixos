614204ffc040cf281ba2eb4f98f68e9264cca975
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-02-10 23:28:39 +0000

    551 current  2026-02-10 23:28:39  26.05.20260204.00c21e4 (Yarara)  6.18.8

--- homeManagerModules/cliPrograms/scripts/rebuild.nix
+++ homeManagerModules/cliPrograms/scripts/rebuild.nix
@@ -5,10 +5,6 @@
       set -e
       pushd ~/nixos/
       # nvim
-      cd nixosSecrets/
-      git add .
-      git commit -m "$gen current  $(date '+%Y-%m-%d %H:%M:%S')  $(nixos-version)  $(uname -r)"
-      cd ..
       nix flake update secrets
       git add .
       git diff --cached -U0 *.nix
