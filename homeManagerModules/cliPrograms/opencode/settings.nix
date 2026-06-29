3c2eb4113dcf141cdd8ecfd7044785b70511e304
lucasgmf <lucasgalvaomfreitas@gmail.com>
2026-06-29 21:36:16 +0100

    trying different models + different local network

--- homeManagerModules/cliPrograms/opencode/settings.nix
+++ homeManagerModules/cliPrograms/opencode/settings.nix
@@ -4,9 +4,9 @@
   ...
 }: {
   programs.opencode.settings = {
-    "$schema" = "https://opencode.ai/config.json";
+    # "$schema" = "https://opencode.ai/config.json";
 
-    model = "ollama/qwen3:8b-64k";
+    model = "ollama/gpt-oss:20b-64k"; # default model for opencode
 
     autoupdate = false;
 
@@ -25,23 +25,23 @@
       webfetch = "ask";
     };
 
-    theme = "material-you";
+    # theme = "material-you";
 
     provider = {
       ollama = {
         npm = "@ai-sdk/openai-compatible";
         name = "Ollama (Windows)";
         options = {
-          baseURL = "http://192.168.1.105:11434/v1";
+          baseURL = "http://192.168.1.88:11434/v1";
         };
         models = {
-          "qwen3:8b-64k" = {
-            name = "Qwen3 8B (64k)";
-            tools = true;
+          "gpt-oss:20b-64k" = {
+            name = "GPT-OSS 20B (64k)";
+            # tools = true;
           };
-          "hermes3:8b-64k" = {
-            name = "Hermes3 8B (64k)";
-            tools = true;
+          "qwen3-coder:30b-64k" = {
+            name = "Qwen3-Coder 30B (64k)";
+            # tools = true;
           };
         };
       };
